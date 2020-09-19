3475e74290b11ea355d37c6c5099bfe3fafa6f1c
shatpolina <shat.polina@gmail.com>
2020-09-19 20:13:15 +0300

    Change type int to type bigInt for num, numps, price

--- clicker.go
+++ clicker.go
@@ -3,65 +3,106 @@ package main
 import (
     "fmt"
     "net/http"
+    "math/big"
 )
 
 type Clicker struct {
     UserID int `json:"id"`
-    Numps int `json:"numps"`
-    Num int `json:"num"`
-    Price_clicker int `json:"price_clicker"`
+    Numps string `json:"numps"`
+    Num string `json:"num"`
+    Price_clicker string `json:"price_clicker"`
     Qty_clicker int `json:"qty_clicker"`
-    Price_super_clicker int `json:"price_super_clicker"`
+    Price_super_clicker string `json:"price_super_clicker"`
     Qty_super_clicker int `json:"qty_super_clicker"`
-    Price_mega_clicker int `json:"price_mega_clicker"`
+    Price_mega_clicker string `json:"price_mega_clicker"`
     Qty_mega_clicker int `json:"qty_mega_clicker"`
 }
 
 func Clicknum(w http.ResponseWriter, r *http.Request) {
     checkSession(w, r, "", "/auth")
     c, _ := r.Cookie("session")
+    
     var userID int
     db.QueryRow("select UserID from sessions where UUID = $1", c.Value).Scan(&userID)
-    var num int
+    
+    var num string
     db.QueryRow("select Num from users where UserID = $1", userID).Scan(&num)
-    num += 1
-    fmt.Fprintf(w, "%d", num)
+    
+    var num_value big.Int
+    num_value.SetString(num, 10)
+    num_value.Add(&num_value, big.NewInt(int64(1)))
+    num = num_value.String()
+    fmt.Fprintf(w, num)
+    
     db.Exec("update users set Num = $1 where UserID = $2", num, userID)
 }
 
 func Clickers(userID int) {
-    var num int
-    var numps int
+    var num string
+    var numps string
     db.QueryRow("select Num, Numps from users where UserID = $1", userID).Scan(&num, &numps)
-    num += numps
+    var num_value big.Int
+    num_value.SetString(num, 10)
+    var numps_value big.Int
+    numps_value.SetString(numps, 10)
+    num_value.Add(&num_value, &numps_value)
+    num = num_value.String()
     db.Exec("update users set Num = $1 where UserID = $2", num, userID)
 }
 
 func Buy(w http.ResponseWriter, r *http.Request, qty_name string, price_name string) {
     checkSession(w, r, "", "/auth")
     c, _ := r.Cookie("session")
+    
     var userID int
     db.QueryRow("select UserID from sessions where UUID = $1", c.Value).Scan(&userID)
-    var num int
-    var numps int
-    var qty_value int
-    var price_value int
+    var num string
+    var numps string
+    var qty int
+    var price string
     var select_text string = "select " + qty_name + ", " + price_name + ", Num, Numps from users where UserID = $1"
-    db.QueryRow(select_text, userID).Scan(&qty_value, &price_value, &num, &numps)
-    num -= price_value
-    if num > 0 {
-        qty_value += 1
-        price_value = int(float64(price_value) * 1.1)
+    
+    db.QueryRow(select_text, userID).Scan(&qty, &price, &num ,&numps)
+    
+    var num_value big.Int
+    num_value.SetString(num, 10)
+    var price_value big.Int
+    price_value.SetString(price, 10)
+    cmp := num_value.Cmp(&price_value)
+    
+    if cmp >= 0 {
+        num_value.Sub(&num_value, &price_value)
+        num = num_value.String()
+        
+        db.Exec("update users set Num = $1 where UserID = $2", num, userID)
+        
+        var price_float big.Float
+        price_float.SetInt(&price_value)
+        var k big.Float
+        k.SetFloat64(1.2)
+        price_float.Mul(&price_float, &k)
+        price_float.Int(&price_value)
+        
+        db.Exec("update users set " + price_name + " = $1 where UserID = $2", price, userID)
+
+        qty += 1
+        var numps_value big.Int
+        numps_value.SetString(numps, 10)
+
         if qty_name == "Qty_clicker" {
-            numps += qty_value * 1
+            numps_value.Add(&numps_value, big.NewInt(int64(1)))
         } else if qty_name == "Qty_super_clicker" {
-            numps += qty_value * 10
+            numps_value.Add(&numps_value, big.NewInt(int64(5)))
         } else if qty_name == "Qty_mega_clicker" {
-            numps += qty_value * 100
+            numps_value.Add(&numps_value, big.NewInt(int64(10)))
         }
         
-        db.Exec("update users set " + qty_name + "= $1," + price_name + "= $2, Num = $3, Numps = $4 where UserID = $5", qty_value, price_value, num, numps, userID)
-        fmt.Fprintf(w, "%d", qty_value)
+        numps = numps_value.String()
+        price = price_value.String()
+        
+        db.Exec("update users set " + qty_name + "= $1," + price_name + "= $2, Numps = $3 where UserID = $4", qty, price, numps, userID)
+        
+        fmt.Fprintf(w, "%d", qty)
     } else {
         fmt.Fprintf(w, "error")
     }  
--- user.go
+++ user.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-     "fmt"
-     "net/http"
-     "io/ioutil"
+    "fmt"
+    "net/http"
 )
 
 type User struct {
     UserID int `json:"id"`
     Login string `json:"login"`
     Password string `json:"password"`
-    Num int `json:"num"`
-    Numps int `json:"numps"`
-    Price_clicker int `json:"price_clicker"`
+    Num string `json:"num"`
+    Numps string `json:"numps"`
+    Price_clicker string `json:"price_clicker"`
     Qty_clicker int `json:"qty_clicker"`
-    Price_super_clicker int `json:"price_super_clicker"`
+    Price_super_clicker string `json:"price_super_clicker"`
     Qty_super_clicker int `json:"qty_super_clicker"`
-    Price_mega_clicker int `json:"price_mega_clicker"`
+    Price_mega_clicker string `json:"price_mega_clicker"`
     Qty_mega_clicker int `json:"qty_mega_clicker"`
     Connected bool `json:"connected"`
 }
 
 func Registration(w http.ResponseWriter, r *http.Request, login string, password string) {
     var userID int
-    db.QueryRow("insert into users (Login, Password, Num, Price_clicker, Qty_clicker, Price_super_clicker, Qty_super_clicker, Price_mega_clicker, Qty_mega_clicker, Numps) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning UserID", login, password, 0, 10, 0, 100, 0, 1000, 0, 0).Scan(&userID)
+    db.QueryRow("insert into users (Login, Password, Num, Price_clicker, Qty_clicker, Price_super_clicker, Qty_super_clicker, Price_mega_clicker, Qty_mega_clicker, Numps) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning UserID", login, password, 0, 100, 0, 1000, 0, 10000, 0, 0).Scan(&userID)
     c, _:= r.Cookie("session")
     db.Exec("update sessions set UserID = $1 where UUID = $2", userID, c.Value)
     http.Redirect(w, r, "/home", http.StatusSeeOther)
